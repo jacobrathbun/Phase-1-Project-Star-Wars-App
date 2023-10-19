0c57cf4e77ffdca2502bbee667fe3e95128c6e48
Jacob Rathbun <jrathofbun@gmail.com>
2023-10-19 00:09:13 -0400

    Basic search functionality added

--- index.js
+++ index.js
@@ -19,6 +19,43 @@ yodaBtn.addEventListener("click", () => {
             `; 
     })
     .catch((error) => {
-        console.error("Error fetching data.", error);
+        console.error("Error fetching character data.", error);
+    });
+});
+
+const searchForm = document.getElementById('searchForm');
+const characterSearchContainer = document.getElementById('characterSearchContainer');
+
+searchForm.addEventListener("submit", (form) => {
+    form.preventDefault();
+
+    const formData = searchForm.querySelector('input').value;
+
+    fetch(`https://swapi.dev/api/people/?search=${formData}`)
+    .then((response) => {
+        if (!response.ok) {
+            throw new Error("Error connection to SWAPI...")
+        }
+        return response.json();
+    })
+    .then((data) => {
+
+        if (data.results && data.results.length > 0) {
+            const resultsHTML = data.results.map((result) => {
+                return `
+                <h2>Name: ${result.name}</h2>
+                <p>Height: ${result.height} cm</p>
+                <p>Mass: ${result.mass} kg</p>
+                <p>Eye Color: ${result.eye_color}</p>
+                <p>Hair Color: ${result.hair_color}</p>
+            `;
+            }).join("");
+            characterSearchContainer.innerHTML = resultsHTML;
+        } else {
+            characterSearchContainer.innerHTML = "No results found...";
+        }
+    })
+    .catch((error) => {
+        console.error("Error fetching search data...", error);
     });
 });
